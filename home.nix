ecfd6acd0c845c51112ea56f380ce3657912106e
Nathan Lo <natefung2012@gmail.com>
2026-04-22 00:43:48 -0400

    added aliases

--- home.nix
+++ home.nix
@@ -1,6 +1,16 @@
 {
     home.stateVersion = "25.11";
 
+    home.shellAliases = {
+        g = "vim";
+        u = "cd ..";
+        uu = "cd ../..";
+        uuu = "cd ../../..";
+        uuuu = "cd ../../../..";
+        rl = "realpath";
+        rebuild = "sudo nixos-rebuild switch --flake .";
+    };
+
     programs.kitty = {
         enable = true;
         themeFile = "midsummer-night";
