95cf87b39d358c11e4480ccde5fd1630b873a215
Nathan Lo <natefung2012@gmail.com>
2026-04-21 23:50:57 -0400

    added theme and moved kitty to home manager. added font to configuration.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Storage server NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations.gallanough = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nate = ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,13 @@
+{
+    home.stateVersion = "25.11";
+
+    programs.kitty = {
+        enable = true;
+        themeFile = "midsummer-night";
+
+        font = {
+            name = "mononoki";
+            size = 12;
+        };
+    };
+}
